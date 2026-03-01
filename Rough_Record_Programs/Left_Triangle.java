3ab2b1da3e8366852e1289912b089a9c3063de09
Rabah Ahmed <88627155+Silvie3D@users.noreply.github.com>
2026-03-02 03:42:53 +0530

    Restructured to SubFolders

--- Rough_Record_Programs/Left_Triangle.java
+++ Rough_Record_Programs/Left_Triangle.java
@@ -1,6 +1,6 @@
 class Left_Triangle{
 	public static void main(String[] args){
-		for ( int i = 1; i < 5; i++){
+		for ( int i = 1; i <= 5; i++){
 			System.out.print(" ".repeat(5-i));
 			System.out.print("*".repeat(i));
 			System.out.print("\n");
